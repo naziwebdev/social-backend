421b65fb8cfa9c0a374ee4ebc0bb45ac2db6a810
nazi <nazipaeez777@gmail.com>
2024-06-08 23:22:38 +0330

    Develope register controller (Not Complete)

--- src/modules/auth/auth.controller.js
+++ src/modules/auth/auth.controller.js
@@ -1,6 +1,35 @@
+const userModel = require("../../models/User");
+const { registerSchemaٰValidator } = require("./auth.validator");
 
+exports.register = async (req, res, next) => {
+  try {
+    const { username, email, name, password } = req.body;
 
+    await registerSchemaٰValidator.validate(
+      { username, email, name, password },
+      {
+        abortEarly: false,
+      }
+    );
 
-exports.register = async (req,res,next) => {
-    
-}
+    const isExistUser = await userModel.findOne({
+      $or: [{ username }, { email }],
+    });
+    if (isExistUser) {
+      return res.status(400).json({ message: "user exist alredy" });
+    }
+
+    let isFirstUser = (await userModel.countDocuments()) === 0;
+    let role = "USER";
+    if (isFirstUser) {
+      role = "ADMIN";
+    }
+
+    user = new userModel({ username, email, name, password, role });
+    user = await user.save();
+
+    return res.status(201).json({ message: "user registerd successfully" });
+  } catch (error) {
+    next(error);
+  }
+};

b9808d60d65eb89d3951e28a952c865fcb255fde
nazi <nazipaeez777@gmail.com>
2024-06-08 22:10:42 +0330

    Config server

--- src/server.js
+++ src/server.js
@@ -1,3 +1,34 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
+const app = require("./app");
+const dotenv = require("dotenv");
 
+const isDevelopmentMode = process.env.NODE_ENV === "development";
+if (isDevelopmentMode) {
+  dotenv.config();
+}
 
+const connectToDB = async () => {
+  try {
+    
+    await mongoose.connect('mongodb://127.0.0.1:27017/social');
+    console.log(`mongodb connected successfully : ${mongoose.connection.host}`);
+  } catch (error) {
+    console.log("mongodb connection faild", error);
+    process.exit(1);
+  }
+};
+
+const sevrverRunning = () => {
+  const port = +process.env.PORT || 4002;
+
+  app.listen(port, () => {
+    console.log(`Sever running on port ${port}`);
+  });
+};
+
+const run = async () => {
+  sevrverRunning();
+  await connectToDB();
+};
+
+run();

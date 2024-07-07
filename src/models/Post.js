66d0fd4ecea039f62b82c7f6fb2bdafba1704539
nazi <nazipaeez777@gmail.com>
2024-07-07 23:35:03 +0330

    Create post model

---
+++ src/models/Post.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+const schema = new mongoose.Schema(
+  {
+    media: {
+      path: { type: String, required: true },
+      filename: { type: String, required: true },
+      required: true,
+    },
+    user: {
+      type: mongoose.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    hashtags: {
+      type: [String],
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const model = mongoose.model("Post", schema);
+
+module.exports = model;

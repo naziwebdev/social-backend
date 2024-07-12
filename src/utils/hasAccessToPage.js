fb77e522cefcf9de51ee77c341204fbf61767edd
nazi <nazipaeez777@gmail.com>
2024-07-12 20:03:58 +0330

    Develope hepler func for access to pages

---
+++ src/utils/hasAccessToPage.js
@@ -0,0 +1,31 @@
+const followModel = require("../models/Follow");
+const userModel = require("../models/User");
+
+module.exports = async (userID, pageID) => {
+  try {
+    if (userID === pageID) {
+      return true;
+    }
+
+    const page = await userModel.findOne({ _id: pageID }).lean();
+
+    if (!page.private) {
+      return true;
+    }
+
+    const followed = await followModel
+      .findOne({
+        follower: userID,
+        following: pageID,
+      })
+      .lean();
+
+    if (!followed) {
+      return false;
+    }
+
+    return true;
+  } catch (error) {
+    console.log(error);
+  }
+};

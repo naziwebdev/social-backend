8a5d51cd040078525f0aec2fe01daa4095c49a95
nazi <nazipaeez777@gmail.com>
2024-07-21 21:00:13 +0330

    Create save model

---
+++ src/models/Save.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose')
+
+const schema =  new mongoose.Schema({
+    user:{
+        type:mongoose.Types.ObjectId,
+        ref:'User',
+        required:true
+    },
+    post:{
+        type:mongoose.Types.ObjectId,
+        ref:'Post',
+        required:true
+    }
+    
+},{timestamps:true})
+
+
+const model = mongoose.model('Save' , schema)
+
+module.exports = model
